05049b72be28830d1baf7a1a66f4a20fc2e2bf23
Dio Ilham <apajabole1@gmail.com>
2021-06-25 23:31:40 +0700

    Update root files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './**/*.html',
-    './**/*.js',
+    // './1-movie-search/**/*.html',
+    // './1-movie-search/**/*.js',
+
+    // './2-omdb-search/**/*.html',
+    // './2-omdb-search/**/*.js',
+
+    './3-vb-todo-list/**/*.html',
+    './3-vb-todo-list/**/*.js',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
