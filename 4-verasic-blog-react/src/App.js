28606d5294760a8659fef91439e5cf833564fb01
Dio Ilham <apajabole1@gmail.com>
2021-07-11 15:46:23 +0700

    #4: Switch BrowserRouter to HashRouter for GithubPages incompatibility with html5 browser history.

--- 4-verasic-blog-react/src/App.js
+++ 4-verasic-blog-react/src/App.js
@@ -1,5 +1,5 @@
 // import { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import useTabTitle from './hooks/UseTabTitle';
 
